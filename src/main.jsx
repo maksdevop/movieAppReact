416c233ae023dc9961246fcff9ff205f4e1627f1
maksdevop <maksmm.sirotin@mail.ru>
2024-12-02 17:32:22 +0300

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,10 @@
 import { createRoot } from 'react-dom/client';
+import React, { StrictMode } from 'react';
 import './index.css';
 import App from './components/App/App';
 
-createRoot(document.getElementById('root')).render(<App />);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
